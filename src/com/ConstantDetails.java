bdb5d63511dd91bcb31cf6e6d27ea05384a84235
Venkat <Venkat@DESKTOP-9EQM6E0>
2020-01-25 07:52:59 +0530

    Added constanet changes

--- src/com/ConstantDetails.java
+++ src/com/ConstantDetails.java
@@ -8,5 +8,9 @@ public class ConstantDetails {
 	static String PWD_PI = "Krishna";
 	static String PWD_TZ = "Krishna";
 	static String PWD_Work_Sheet = "Krishna";
-
+	public static String SELECT_STD_TB = "select * from std_tb";
+	public static String Oracle_Driver = "oracle.jdbc.driver.OracleDriver";
+	public static String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe";
+	public static String USER_NAME = "System";
+	public static String SQL_PWD = "System";
 }
